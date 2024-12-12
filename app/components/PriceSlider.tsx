773c2c552c04ad349b42660a6884b77bd7d3dc63
George Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2024-12-12 22:57:05 +0530

    Interval 5 Seconds

--- app/components/PriceSlider.tsx
+++ app/components/PriceSlider.tsx
@@ -11,7 +11,7 @@ export const PriceSlider = ({ pages }: { pages: Page[] }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % pages.length);
-    }, 15000); // Rotate every 10 seconds
+    }, 5000); // Rotate every 10 seconds
 
     return () => clearInterval(interval);
   }, [pages.length]);
